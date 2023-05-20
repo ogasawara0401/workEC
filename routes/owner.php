a70139262f18ba11f02baf322e439c491c8fdee9
yuki <y.ogasawara0401@gmail.com>
2023-05-21 02:44:08 +0900

    no-resizing upload

--- routes/owner.php
+++ routes/owner.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
   return view('owner.welcome');
 });
 
-Route::prefix('shop')->middleware('auth:owners')->group(function () {
+Route::prefix('shops')->middleware('auth:owners')->group(function () {
   Route::get('index', [ShopController::class, 'index'])->name('shops.index');
   Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
   Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
